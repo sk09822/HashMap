2baa7b5685e23f6cce4bfa75d6cf67f818684ee6
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-07 12:08:23 +0530

    HashMap Practice Problem

---
+++ src/com/blz/HashMap/HashMap.java
@@ -0,0 +1,5 @@
+package com.blz.HashMap;
+
+public class HashMap {
+
+}
---
+++ src/com/blz/HashMap/Main.java
@@ -0,0 +1,10 @@
+package com.blz.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to HashMap Program !!!");
+
+	}
+
+}
