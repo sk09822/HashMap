71aa040b73a8b2dd680fc1bf3f4e300ea3e4231d
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-07 12:39:36 +0530

    Added : find the frequency of large paragraph

--- src/com/blz/HashMap/Main.java
+++ src/com/blz/HashMap/Main.java
@@ -3,7 +3,7 @@ package com.blz.HashMap;
 public class Main {
 	public static void main(String[] args) {
 		HashMap map = new HashMap();
-		map.countFreq("To be or not to be");
+		map.countFreq("paranoids are not paranoids because they are paranoid but they keep putting themselves deliberately into paranoid avoidable situation");
 
 	}
 }
