78a5b8862b8286f03dddde0de32bd353304e2d68
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-07 12:35:50 +0530

    Added : find a frequency of words in sentence

--- src/com/blz/HashMap/HashMap.java
+++ src/com/blz/HashMap/HashMap.java
@@ -1,5 +1,33 @@
 package com.blz.HashMap;
 
-public class HashMap {
+import java.util.Map;
+import java.util.TreeMap;
 
+class HashMap {
+	public void countFreq(String str) {
+
+		Map<String, Integer> map = new TreeMap<>();
+
+		String arr[] = str.split(" ");
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (map.containsKey(arr[i])) {
+
+				map.put(arr[i], map.get(arr[i]) + 1);
+			}
+
+			else {
+
+				map.put(arr[i], 1);
+			}
+		}
+
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+
+			System.out.println(entry.getKey() + " -> " + entry.getValue());
+		}
+	}
 }
+
+	
--- src/com/blz/HashMap/Main.java
+++ src/com/blz/HashMap/Main.java
@@ -1,10 +1,9 @@
 package com.blz.HashMap;
 
 public class Main {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome to HashMap Program !!!");
+		HashMap map = new HashMap();
+		map.countFreq("To be or not to be");
 
 	}
-
 }
